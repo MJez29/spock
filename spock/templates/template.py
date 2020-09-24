0ae45c76c8bc4d4671b21638eab21f4bf97a4964
Callum Moseley <callum.moseley1@gmail.com>
2020-09-24 11:49:22 -0400

    Fixed relative path name issue

--- spock/templates/template.py
+++ spock/templates/template.py
@@ -1,3 +1,6 @@
+from importlib import resources as pkg_resources
+from spock import templates
+
 authorizedHtml = None
 errorHtml = None
 
@@ -5,12 +8,12 @@ errorHtml = None
 def get_error_page(error_code):
     global errorHtml
     if not errorHtml:
-        errorHtml = open("spock/templates/error.html").read()
+        errorHtml = pkg_resources.open_text(templates, "error.html").read()
     return errorHtml.replace("<error_code>", error_code).encode()
 
 
 def get_authorized_page():
     global authorizedHtml
     if not authorizedHtml:
-        authorizedHtml = open("spock/templates/authorized.html").read()
+        authorizedHtml = pkg_resources.open_text(templates, "authorized.html").read()
     return authorizedHtml.encode()
